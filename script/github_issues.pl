e05f57e82792f3c7f6e7b1d8f032e42c6136746a
skaufman <sam@socialflow.com>
2013-09-11 13:54:16 -0400

    repo and user

--- script/github_issues.pl
+++ script/github_issues.pl
@@ -3,10 +3,18 @@ use warnings FATAL => 'all';
 use Pithub;
 use Data::Dumper::Concise;
 my $api_token = $ENV{GH_API_TOKEN} or die '$ENV{GH_API_TOKEN} required';
-my $pithub = Pithub->new( token => $api_token );
+
+my $user = $ARGV[0];
+my $repo = $ARGV[1];
+my $pithub =
+  Pithub->new( token => $api_token, user => $user, repo => $repo );
 sub format_bug {
     my $issue = shift;
-    return sprintf( "[ %s ] [ %s ]\n%s\n", $issue->{state}, $issue->{title}, $issue->{html_url} );
+    return sprintf( "[ created by: %s ] [ assigned to: %s ] [ %s ]\n%s\n",
+        $issue->{user}{login},
+        ( $issue->{assignee} ? $issue->{assignee}{login} : "N/A" ),
+        $issue->{title},
+        $issue->{html_url} );
 }
 my $issues = $pithub->issues->list;
 while( my $issue = $issues->next ) {
